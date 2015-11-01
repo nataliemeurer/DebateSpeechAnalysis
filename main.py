aca191c4fce9403e336044f389109f2e54e075c4
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-11-01 16:17:53 -0500

    Refactor to use wordcloud.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import re
 import codecs
-import pywordcloud as pyw
+import numpy as np
+from os import path
+import wordcloud as wc
 
 
-fileList = ['transcripts/democrats_10_13_2015', 'transcripts/republicans_10_13_2015']
+fileList = ['transcripts/democrats_10_13_2015', 'transcripts/republicans_10_28_2015']
 speakerOutputNames = ['trump', 'fiorina', 'sanders', 'clinton', 'carson', 'o\'malley', 'kasich']
 speaker_words = {}
 
@@ -13,31 +15,21 @@ for fi in fileList:
 	current_line = transcript_file.readline()
 	while current_line != '':
 		if re.search(r'[A-Z]+\:', current_line):
-			current_line = current_line.replace('\n', ' ')
+			current_line = current_line.replace('\n', ' ').replace('\u', '')
 			split_line = current_line.split(": ")
 			if split_line[0].lower() in speaker_words:
 				speaker_words[split_line[0].lower()] += split_line[1]
 			else:
 				speaker_words[split_line[0].lower()] = split_line[1]
-			current_line = transcript_file.readline().replace('/n', ' ')
+			current_line = transcript_file.readline().replace('/n', ' ').replace('\u', '')
 			while not re.search(r'[A-Z]+\:', current_line):
 				speaker_words[split_line[0].lower()] += current_line
-				current_line = transcript_file.readline().replace('/n', ' ')
+				current_line = transcript_file.readline().replace('/n', ' ').replace('\u', '')
 				if current_line == '':
 					break
 
 for name in speakerOutputNames:
-	pyw.create(speaker_words[name],
-			outfile="output/" + name + ".html",
-			uppercase=False,
-			showfreq=False,
-			frequency=100,
-			removepunct = True,
-			minfont = 1,
-			maxfont = 8, 
-	        hovercolor="green", 
-	        showborder=False, 
-	        fontfamily='calibri', 
-	        width="1100px", 
-	        height="400px"
-	        )
+	# take relative word frequencies into account, lower max_font_size
+	wordcloud = wc.WordCloud(max_font_size=40).generate(speaker_words[name])
+	image = wordcloud.to_image()
+	image.show()
